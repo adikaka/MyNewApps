1b49c68ecb25aa892082a0e80de3048aa0ab880e
adikaka <budimargono170764@gmail.com>
2024-01-31 16:07:16 +0700

    first commit

---
+++ app/src/main/java/com/example/mynewsapplication/utils/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.mynewsapplication.utils
+
+class Constants {
+    companion object {
+        const val API_KEY = "dc321f4f20104bc18156d2a0319deee0"
+        const val BASE_URL = "https://newsapi.org"
+//        const val SEARCH_NEWS_TIME_DELAY = 500L
+//        const val QUERY_PAGE_SIZE = 20
+    }
+}
